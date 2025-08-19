b5ef83fcc151e5c454e45d44fef193163cbfa401
alafdili <alafdili@inception.inception>
2025-08-19 11:08:14 -0400

    compose file is created for the three main services.

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 
-mariadb-install-db --user=mysql --datadir=/mnt/db/
-
-mariadbd --user=mysql &
-
-sleep 10
+mariadb-install-db
 
 ROOT_PASSWD=$(cat $DB_ROOT_PASSWD_FILE)
 USER_PASSWD=$(cat $DB_PASSWD_FILE)
 
-mariadb --socket=/run/mysqld/mysqld.sock \
-        -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASSWD';"
+cat << EOF > /run/startupQueries.sql
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASSWD';
+        CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$USER_PASSWD';
+        GRANT ALL PRIVILEGES ON $DB_NAME.* TO $MYSQL_USER;
+        FLUSH PRIVILEGES;
+EOF
 
-mariadb -u root \
-        -p$ROOT_PASSWD \
-        -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$USER_PASSWD';"
-
-mariadb -u root \
-        -p$ROOT_PASSWD \
-        -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $MYSQL_USER;"
-
-mariadb -u root \
-        -p$ROOT_PASSWD \
-        -e "FLUSH PRIVILEGES"
-
-PID=$(ps -C mariadbd -o pid=)
-kill $PID
-
-sleep 10
-
-exec mariadbd --user=mysql
+exec mariadbd
